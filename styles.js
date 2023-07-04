e4b8e55c3b0a13cc5f3a10e0b5e66ee19c14144a
AmazingBandit <sa.rajbhandari@gmail.com>
2023-07-04 00:16:13 -0700

    Completed Rock, Paper, Scissor to play 5 games

--- styles.js
+++ styles.js
@@ -6,30 +6,66 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
+  computerSelection = getComputerChoice();
+  playerSelection = prompt(
+    "Type your selection. (Rock, Paper, or Scissor"
+  ).toLowerCase();
+  console.log(playerSelection);
+
   if (
-    playerSelection == "rock" ??
-    playerSelection == "paper" ??
+    playerSelection == "rock" ||
+    playerSelection == "paper" ||
     playerSelection == "scissor"
   ) {
     if (playerSelection == "rock") {
       if (computerSelection == "rock") {
-        console.log("You Tied! Rock vs. Rock");
+        return console.log(`You Tied! Rock vs. Rock | Score: ${score}`);
       } else if (computerSelection == "paper") {
-        console.log("You Lose! Paper beats Rock");
+        return console.log(`You Lose! Paper beats Rock | Score: ${score}`);
       } else {
-        console.log("You Win! Rock beats Scissor");
+        score++;
+        return console.log(`You Win! Rock beats Scissor | Score: ${score}`);
+      }
+    } else if (playerSelection == "paper") {
+      if (computerSelection == "rock") {
+        return console.log(`You Win! Paper beats Rock | Score: ${score}`);
+      } else if (computerSelection == "paper") {
+        return console.log(`You Tied! Paper vs. Paper | Score: ${score}`);
+      } else {
+        return console.log(`You Lose! Scissor beats Paper | Score: ${score}`);
+      }
+    } else {
+      if (computerSelection == "rock") {
+        return console.log(`You Lose! Rock beats Scissor | Score: ${score}`);
+      } else if (computerSelection == "paper") {
+        return console.log(`You Win! Scissor beats Paper | Score: ${score}`);
+      } else {
+        return console.log(`You Tied! Scissor vs. Scissor | Score: ${score}`);
       }
     }
-  }
-  else {
-    alert('please enter valid input');
+  } else {
+    alert("please enter valid input");
   }
 }
 
-const playerSelection = prompt("Type your selection. (Rock, Paper, or Scissor");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));
+  if(score >= 3) {
+    console.log(`You WIN! You won ${score}/5 games! Congratulations`)
+  }
+  else {
+    console.log(`You LOSE! You only won ${score}/5 games. Better luck next time.`)
+  }
+}
+
+let playerSelection;
+let computerSelection;
+let score = "0";
+game();
 
 //     store random choice
 //     get user choice
